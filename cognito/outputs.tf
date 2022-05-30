1cbeb06a7e0bb2e7816597e33dddcae543feb3fb
Giacomo Aluigi <giacomo.aluigi@gmail.com>
2022-05-30 15:44:37 +0200

    add cognito module

---
+++ cognito/outputs.tf
@@ -0,0 +1,19 @@
+output "user_pool_id" {
+  value = aws_cognito_user_pool.kibana.id
+}
+
+output "identity_pool_id" {
+  value = aws_cognito_identity_pool.kibana.id
+}
+
+output "role_authenticated" {
+  value = aws_iam_role.kibana_limited_auth.arn
+}
+
+output "role_unauthenticated" {
+  value = aws_iam_role.kibana_unauth.arn
+}
+
+output "kibana_endpoint" {
+  value = aws_cognito_user_pool.kibana.endpoint
+}
